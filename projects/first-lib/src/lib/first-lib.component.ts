803f5c7cfef03226a395586b7479816f562c168c
alptekin-mert <mrtlptkn45@gmail.com>
2023-07-14 16:45:48 +0300

    AngularWorkSpace

---
+++ projects/first-lib/src/lib/first-lib.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'firstLibComponent',
+  template: ` <p>first-lib works!</p> `,
+  styles: [],
+})
+export class FirstLibComponent {}
